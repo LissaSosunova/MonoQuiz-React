8f9324d927f089fa56e8d33e4a82141e5cafc85a
Alisa Sosunova <alisasosunova@MacBook-Pro-Alisa.local>
2026-01-23 13:26:05 +0200

    add admin pannel table

--- src/api/users.api.ts
+++ src/api/users.api.ts
@@ -8,4 +8,10 @@ export const UsersAPI = {
   getUser() {
     return http.get<User>("/users/user");
   },
+  updateRole(id: string, role: string) {
+    return http.patch<User>(`/users/changerole/${id}/role`, {role});
+  },
+  updateActive(id: string, isActive: boolean) {
+    return http.patch<User>(`/users/changevisibility/${id}/isActive`, { isActive});
+  }
 };
