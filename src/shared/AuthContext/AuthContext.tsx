689408e292742e86284a6bffc15edfae3cf88053
Alisa Sosunova <alisasosunova@MacBook-Pro-Alisa.local>
2026-01-23 11:59:21 +0200

    add toasts

--- src/shared/AuthContext/AuthContext.tsx
+++ src/shared/AuthContext/AuthContext.tsx
@@ -1,42 +1,57 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { UsersAPI } from '../../api/users.api'
 import { type User } from '../../shared/interfaces/User'
+import { showToast } from '../../shared/ui/toast'
 
 
 interface AuthContextType {
-  user: User | null
-  loading: boolean
+    user: User | null
+    loading: boolean
+    login: (token: string, user: User) => void
+    logout: () => void
 }
 
-const AuthContext = createContext<AuthContextType>({
-  user: null,
-  loading: true,
-})
+const AuthContext = createContext<AuthContextType | null>(null)
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
-  const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (!token) {
-      setLoading(false)
-      return
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            setLoading(false)
+            return
+        }
+
+        UsersAPI.getUser()
+            .then(res => setUser(res.data))
+            .catch(() => {
+                localStorage.removeItem('token')
+            })
+            .finally(() => setLoading(false))
+    }, [])
+        const login = (token: string, user: User) => {
+        localStorage.setItem('token', token)
+        showToast.success(`Welcome back ${user.name}`)
+        console.log(`Welcome back ${user.name}`)
+        setUser(user)
     }
 
-    UsersAPI.getUser()
-      .then(res => setUser(res.data))
-      .catch(() => {
+    const logout = () => {
         localStorage.removeItem('token')
-      })
-      .finally(() => setLoading(false))
-  }, [])
+        setUser(null)
+    }
 
-  return (
-    <AuthContext.Provider value={{ user, loading }}>
-      {children}
-    </AuthContext.Provider>
-  )
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+    const ctx = useContext(AuthContext)
+    if (!ctx) throw new Error('useAuth must be used within AuthProvider')
+    return ctx
+}
